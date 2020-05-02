8055183714fba7df5bf51c3f07d6362b99f7d29e
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-02 13:24:11 +0700

    completing website and add some javascript

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const navbar = document.getElementById('navbar')
+const scrolled = false
+
+// Navbar animation
+window.onscroll = () => {
+  if(window.pageYOffset > 100) {
+    navbar.classList.remove('top')
+    if(!scrolled) {
+      navbar.style.transform = 'translateY(-70px)'
+    }
+    setTimeout(() => {
+      navbar.style.transform = 'translateY(0)'
+      scrolled = true
+    }, 200)
+  } else {
+    navbar.classList.add('top')
+    scrolled = false
+  }
+}
+
+// Smooth Scroll
+$('#navbar a, .btn').on('click', function(e) {
+  if(this.hash !== '') {
+    e.preventDefault()
+  }
+
+  const hash = this.hash
+  
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top - 100
+  }, 800)
+})
